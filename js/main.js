99ff9dbfd718489187c9eb2287763b40ca2f576b
neizerjemima <113371239+neizerjemima@users.noreply.github.com>
2023-10-18 22:43:05 -0400

    done

--- js/main.js
+++ js/main.js
@@ -15,3 +15,38 @@
         button.addEventListener("click", hamburgerMenu, false);
     })();
 })();
+
+// (() => {
+//     const heroImage = gsap.utils.toArray('.hero-image');
+
+//     heroImage.forEach(box => {
+//   gsap.to('.hero-image', { // this will animate ALL boxes
+//     x: -300,
+//     scrollTrigger: {
+//       trigger: '.hero-image',  // this will use the first box as the trigger
+//       scrub: true
+//     }
+//   })
+// });
+    
+// })();
+(() => {
+const boxes = gsap.utils.toArray('.hero-image');
+boxes.forEach(box => {
+  const anim = gsap.to(box, { x: 300, paused: true });
+  
+  heroImage.forEach(box => 
+    gsap.to('.hero-image',
+    ScrollTrigger.create({
+    trigger: box,
+    start: "left 70%",
+    onEnter: () => anim.play()
+  });
+  
+  ScrollTrigger.create({
+    trigger: box,
+    start: "right left",
+    onLeaveBack: () => anim.pause(0)
+  });
+});
+});
